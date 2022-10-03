048a3330682c720a05989507fde84bfaf8ed06cd
Mir-Fayek-Hossain <mirfayekhossain@gmail.com>
2022-10-03 12:42:48 +0600

    working

--- src/app/fetchIntercept.ts
+++ src/app/fetchIntercept.ts
@@ -1,4 +1,5 @@
 import { ENV } from "$src/environment"
+import config from "vite.config";
 
 const baseURL=ENV.CORE_ENDPOINT
 interface Config {
@@ -6,11 +7,20 @@ interface Config {
     headers?: any;
 }
 class _CustomFetch{
-    post=async(as:string,d:Config)=>{
+    post=async(url:string,config?:Config)=>{
+        const bearerToken=await config?.headers?.Authorization?.split(" ");
+        const token = bearerToken[1];
+        if(token){
+            const validToken=
+        }
         return baseURL
     }
     get=async()=>{
         return baseURL
+    }
+    private checkToken=async()=>{
+        const userInfo=JSON.parse(getCookie("xalian_"))
+
     }
 
 }
